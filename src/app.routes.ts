57b0056ae57f6e8da1ed5be70604f88a72750d12
leonardo rivas <84354110+lrivas3@users.noreply.github.com>
2025-02-22 23:32:15 -0600

    :tada: base commit

---
+++ src/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Notfound } from './app/modules/notfound/notfound';
+import { LoginComponent } from './app/modules/auth/LoginComponent/login.component';
+
+export const appRoutes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    { path: 'notfound', component: Notfound },
+    { path: 'auth', loadChildren: () => import('./app/modules/auth/auth.routes') },
+    { path: '**', redirectTo: '/notfound' }
+];
