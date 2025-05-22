c57094aeb352c8a347877a263bf5d959f747917e
Leonardo Rivas <84354110+lrivas3@users.noreply.github.com>
2025-05-21 19:34:36 -0600

    cambiar referencia a backend prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'http://backendhelppoitn:3000',
+    API_URL: '/api',
     recaptchaSiteKey: '6Le_wu4qAAAAAKNDTr0QP--43Trm62d1ATFOjmqx'
 };
